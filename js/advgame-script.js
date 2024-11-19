104e73785b31cd1877709328b585ce2c3d21451f
mateonation <mattfernandezrivera@gmail.com>
2024-11-19 18:26:58 +0100

    user can now move it's block on the grid by pressing 'WASD' keys (WIP)

--- js/advgame-script.js
+++ js/advgame-script.js
@@ -3,11 +3,16 @@ let cells=[];
 let gameGrid=document.querySelector('.game-grid');
 const xinitial=-25;const yinitial=12;
 let x=xinitial;let y=yinitial;
+playerLocation="0/0";
 
 window.onload=function(){
+    //Generate cells
     genCells();
+    // Locate player on the grid
+    locatePlayer();
 }
 
+// Function to generate the grid cells
 function genCells(){
     for(i=0;i<totalCells;i++){
         let div=document.createElement('div');
@@ -15,21 +20,7 @@ function genCells(){
         // Add XY position
         div.id=x+"/"+y;
         cells.push(div);
-        // THIS WILL BE DELETED LATER
-        // Mark X and Y axis
-        div.style.color='white';
-        if(x===0){
-            div.style.background='blue';
-            div.textContent='X';
-        }else if(y===0){
-            div.style.background='red';
-            div.textContent='Y';
-        }
-        if(x===0 && y===0){
-            div.style.background='gold';
-            div.textContent='0';
-        }
-
+        // Show cell on the screen
         gameGrid.append(cells[i]);
         // If X gets to the final element of the row
         if(x===(xinitial*-1)-1){
@@ -44,3 +35,42 @@ function genCells(){
         }
     }
 }
+
+// Function to locate the player
+function locatePlayer(){
+    let playercell=document.getElementById(playerLocation);
+    playercell.classList.add('player');
+}
+
+// FUNCTION FOR WHEN USER PRESSES A WASD KEY
+document.body.addEventListener("keydown", (control)=>{
+    // Execute when one of these keys are pressed
+    if(control.key=='w' || control.key=='a' || control.key=='s' || control.key=='d'){
+        // Read current position of the player
+        let player=document.getElementById(playerLocation);
+        position=player.id.split("/"); //["X","Y"]
+        x=position[0];
+        y=position[1];
+        switch(control.key){
+            // Move up
+            case 'w':
+                y++;
+                break;
+            // Move down
+            case 's':
+                y--;
+                break;
+            // Move left
+            case 'a':
+                x--;
+                break;
+            // Move right
+            case 'd':
+                x++;
+                break;
+        }
+        playerLocation=x+"/"+y;
+        player.classList.remove('player');
+        locatePlayer();
+    }
+});
