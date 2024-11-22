d82a6a18eb3a7897e45d7c1c535a465f6fe6d2ff
mateonation <mattfernandezrivera@gmail.com>
2024-11-22 09:49:25 +0100

    prototype of the first level made on AdventureGame-painter

--- js/advgame-script.js
+++ js/advgame-script.js
@@ -6,36 +6,15 @@ let playerisdead=false;
 
 // ELEMENTS FROM EACH LEVEL
 // COMMON VALUES AND ARRAYS
-let level;
-let spawnpoint=["0/-9"];
-let goal=["-10/7"];
-let doorbutton=["-8/-6"];
-// LEVEL 0
-let wall0=[
-    "8/9","9/9","10/9","11/9","12/9","13/9","14/9",
-    "8/8","9/8","10/8","11/8","12/8","13/8","14/8",
-    "8/7","9/7","10/7","11/7","12/7","13/7","14/7",
-    "8/6","9/6","10/6","11/6","12/6","13/6","14/6",
-    ];
-let lava0=[
-    "8/-9","9/-9","10/-9","11/-9","12/-9","13/-9","14/-9",
-    "8/-8","9/-8","10/-8","11/-8","12/-8","13/-8","14/-8",
-    "8/-7","9/-7","10/-7","11/-7","12/-7","13/-7","14/-7",
-    "8/-6","9/-6","10/-6","11/-6","12/-6","13/-6","14/-6",
-    ];
-let door0=[
-    "-16/9","-15/9","-14/9","-13/9","-12/9","-11/9","-10/9","-9/9","-8/9",
-    "-16/8","-8/8",
-    "-16/7","-8/7",
-    "-16/6","-8/6",
-    "-16/5","-8/5",
-    "-16/4","-15/4","-14/4","-13/4","-12/4","-11/4","-10/4","-9/4","-8/4"
-    ];
+let level=1;
+let spawnpoint=[];
+let goal=[];
+let doorbutton=[];
+let wall=[];
+let lava=[];
+let door=[];
 
 window.onload=function(){
-    //TESTING
-    // Set level number when page is loaded
-    level=0;
     //Generate game
     genGame();
 }
@@ -83,35 +62,69 @@ function genGame(){
 
 // Function to edit cells according to the actual level
 function modifyCell(cell,levelnum){
+    // UNIQUE ELEMENTS FOR EVERY LEVEL
+    spawnpoint=["0/-9","-5/-12"];
+    goal=["-10/7","25/-12"];
+    doorbutton=["-8/-6","-12/-2"];
     switch(levelnum){
         // If level number is not included in this switch => it only generates cells
         default:
-            cell.classList.add('cell');
-            return cell;
+            break;
         // Level 0
         case 0:
-            // Add 'wall' class to the cell (if it's included on the walls' array associated to it's level)
-            if(wall0.includes(cell.id)){
-                cell.classList.add('wall');
-            }
-            // Add 'lava' class to the cell (if it's included on the lava's array associated to it's level)
-            else if(lava0.includes(cell.id)){
-                cell.classList.add('lava');
-            }
-            // Add 'door' class to the cell (if it's included on the door's array associated to it's level)
-            else if(door0.includes(cell.id)){
-                cell.classList.add('door');
-            }
-            // Add 'door-button' class to the cell (if it's id coincides with the one almacenated on the level number position of the door button's array)
-            
-            // Add 'goal' class to the cell (if it's id coincides with the one almacenated on the level number position of the goal's array)
-            
-            // Add 'cell' class to the cell if no other condition has been met
-            else{
-                cell.classList.add('cell');
-            }
-            return cell;
+            // WALL, LAVA AND DOOR ARRAYS FOR THE LEVEL 0 SHOULD BE ADDED HERE
+            wall=[
+                "8/9","9/9","10/9","11/9","12/9","13/9","14/9",
+                "8/8","9/8","10/8","11/8","12/8","13/8","14/8",
+                "8/7","9/7","10/7","11/7","12/7","13/7","14/7",
+                "8/6","9/6","10/6","11/6","12/6","13/6","14/6",
+                ];
+            lava=[
+                "8/-9","9/-9","10/-9","11/-9","12/-9","13/-9","14/-9",
+                "8/-8","9/-8","10/-8","11/-8","12/-8","13/-8","14/-8",
+                "8/-7","9/-7","10/-7","11/-7","12/-7","13/-7","14/-7",
+                "8/-6","9/-6","10/-6","11/-6","12/-6","13/-6","14/-6",
+                ];
+            door=[
+                "-16/9","-15/9","-14/9","-13/9","-12/9","-11/9","-10/9","-9/9","-8/9",
+                "-16/8","-8/8",
+                "-16/7","-8/7",
+                "-16/6","-8/6",
+                "-16/5","-8/5",
+                "-16/4","-15/4","-14/4","-13/4","-12/4","-11/4","-10/4","-9/4","-8/4"
+                ];
+            break;
+        case 1:
+            wall=["-3/9","-2/9","-1/9","0/9","1/9","2/9","3/9","-4/8","-4/7","-1/7","0/7","1/7","2/7","3/7","17/7","18/7","19/7","-4/6","-2/6","3/6","16/6","20/6","-4/5","-2/5","3/5","16/5","20/5","-4/4","-2/4","3/4","8/4","9/4","10/4","11/4","12/4","16/4","20/4","-4/3","-2/3","3/3","7/3","13/3","16/3","20/3","-4/2","-2/2","3/2","7/2","13/2","16/2","19/2","-3/1","3/1","7/1","10/1","13/1","14/1","15/1","16/1","18/1","20/1","-12/0","-11/0","-10/0","3/0","7/0","10/0","18/0","20/0","-13/-1","-9/-1","-6/-1","3/-1","7/-1","10/-1","13/-1","14/-1","18/-1","20/-1","-13/-2","-9/-2","-6/-2","3/-2","7/-2","11/-2","12/-2","15/-2","18/-2","20/-2","-8/-3","-7/-3","-2/-3","-1/-3","0/-3","1/-3","2/-3","7/-3","13/-3","15/-3","18/-3","20/-3","-9/-4","-6/-4","7/-4","10/-4","13/-4","15/-4","18/-4","20/-4","-9/-5","-6/-5","2/-5","3/-5","4/-5","5/-5","6/-5","10/-5","13/-5","15/-5","18/-5","20/-5","-9/-6","-6/-6","0/-6","1/-6","10/-6","13/-6","15/-6","17/-6","20/-6","-9/-7","-6/-7","-1/-7","4/-7","5/-7","6/-7","7/-7","8/-7","9/-7","15/-7","20/-7","-10/-8","-9/-8","-6/-8","-5/-8","-1/-8","10/-8","14/-8","19/-8","-11/-9","-4/-9","0/-9","1/-9","2/-9","3/-9","4/-9","5/-9","6/-9","7/-9","11/-9","12/-9","13/-9","19/-9","-11/-10","-4/-10","10/-10","20/-10","21/-10","22/-10","23/-10","24/-10","25/-10","-11/-11","-6/-11","-5/-11","0/-11","1/-11","2/-11","3/-11","4/-11","5/-11","6/-11","7/-11","8/-11","9/-11","-10/-12","-9/-12","-8/-12","-7/-12","-1/-12"];
+            lava=["-11/12","-10/12","-9/12","-8/12","-7/12","-6/12","-5/12","-4/12","-3/12","-2/12","-1/12","-23/11","-22/11","-21/11","-20/11","-12/11","-11/11","-10/11","-9/11","-8/11","-7/11","-6/11","-5/11","-4/11","-3/11","-2/11","-1/11","0/11","1/11","-23/10","-22/10","-21/10","-20/10","-14/10","-13/10","-12/10","-11/10","-10/10","-9/10","-8/10","-7/10","-6/10","-5/10","-4/10","-3/10","-2/10","-1/10","0/10","1/10","2/10","3/10","-23/9","-22/9","-21/9","-20/9","-15/9","-14/9","-13/9","-12/9","-11/9","-10/9","-9/9","-8/9","-7/9","-6/9","-5/9","-23/8","-22/8","-21/8","-20/8","-15/8","-14/8","-13/8","-12/8","-11/8","-10/8","-9/8","-8/8","-7/8","-6/8","-5/8","-23/7","-22/7","-21/7","-20/7","-15/7","-14/7","-13/7","-12/7","-11/7","-10/7","-9/7","-8/7","-7/7","-6/7","-5/7","-23/6","-22/6","-21/6","-20/6","-15/6","-14/6","-13/6","-12/6","-11/6","-10/6","-9/6","-8/6","-7/6","-6/6","-5/6","-23/5","-22/5","-21/5","-20/5","-15/5","-14/5","-13/5","-12/5","-11/5","-10/5","-9/5","-8/5","-7/5","-6/5","-5/5","-23/4","-22/4","-21/4","-20/4","-14/4","-13/4","-12/4","-11/4","-10/4","-9/4","-8/4","-23/3","-22/3","-21/3","-20/3","-23/2","-22/2","-21/2","-20/2","-23/1","-22/1","-21/1","-20/1","-23/0","-22/0","-21/0","-20/0","-23/-1","-22/-1","-21/-1","-20/-1","-23/-2","-22/-2","-21/-2","-20/-2","-11/-2","-23/-3","-22/-3","-21/-3","-20/-3","-12/-3","-11/-3","-23/-4","-22/-4","-21/-4","-20/-4","-16/-4","-15/-4","-12/-4","-11/-4","-23/-5","-22/-5","-21/-5","-20/-5","-17/-5","-16/-5","-15/-5","-14/-5","-12/-5","-11/-5","-23/-6","-22/-6","-21/-6","-20/-6","-18/-6","-17/-6","-16/-6","-15/-6","-14/-6","-13/-6","-12/-6","-23/-7","-22/-7","-21/-7","-20/-7","-19/-7","-18/-7","-17/-7","-16/-7","-15/-7","-14/-7","-13/-7","-23/-8","-22/-8","-21/-8","-20/-8","-19/-8","-18/-8","-17/-8","-16/-8","-15/-8","-14/-8","-13/-8","-23/-9","-22/-9","-21/-9","-20/-9","-19/-9","-18/-9","-17/-9","-16/-9","-15/-9","-14/-9","-23/-10","-22/-10","-21/-10","-20/-10","-19/-10","-18/-10","-17/-10","-16/-10","-15/-10","-23/-11","-22/-11","-21/-11","-20/-11","-19/-11","-18/-11","-17/-11","-16/-11","-15/-11"];
+            door=["10/12","10/11","10/10","10/9","10/8","10/7","10/6","7/5","8/5","9/5","10/5","4/4","5/4","6/4","7/4","8/1","9/1"];
+            break;
     }
+    // Add 'wall' class to the cell (if it's included on the walls' array associated to it's level)
+    if(wall.includes(cell.id)){
+        cell.classList.add('wall');
+    }
+    // Add 'lava' class to the cell (if it's included on the lava's array associated to it's level)
+    else if(lava.includes(cell.id)){
+        cell.classList.add('lava');
+    }
+    // Add 'door' class to the cell (if it's included on the door's array associated to it's level)
+    else if(door.includes(cell.id)){
+        cell.classList.add('door');
+    }
+    // Add 'door-button' class to the cell (if it's id coincides with the one almacenated on the level number position of the door button's array)
+    else if(doorbutton[levelnum]===cell.id){
+        cell.classList.add('door-button');
+    }
+    // Add 'goal' class to the cell (if it's id coincides with the one almacenated on the level number position of the goal's array)
+    else if(goal[levelnum]===cell.id){
+        cell.classList.add('goal');
+    }
+    // Add 'cell' class to the cell if it can be assigned
+    else{
+        cell.classList.add('cell');
+    }
+    return cell;
 }
 // FUNCTION TO LOCATE PLAYER ON THE GRID
 function locatePlayer(given){
@@ -127,8 +140,8 @@ function locatePlayer(given){
     // If the next cell is a door button, it activates the door opening function
     if(playercell.classList.contains('door-button')){
         playercell.classList.remove('door-button');
-        // Open the door according to it's level number
-        openDoor(level);
+        // Open the doors of the grid
+        openDoor();
     }else{
         // Remove 'cell' class from the cell the player is going to be
         playercell.classList.remove('cell');
@@ -251,16 +264,10 @@ function playerDies(given){
 }
 
 // FUNCTION THAT OPEN DOORS
-function openDoor(levelnum){
-    switch(levelnum){
-        // Open door from level 0
-        case 0:
-            // For every door removed, add a 'cell' class
-            for(i=0;i<door0.length;i++){
-                let openthisdoor=document.getElementById(door0[i]);
-                openthisdoor.classList.remove('door');
-                openthisdoor.classList.add('cell');
-            }
-            break;
+function openDoor(){
+    for(i=0;i<door.length;i++){
+        let openthisdoor=document.getElementById(door[i]);
+        openthisdoor.classList.remove('door');
+        openthisdoor.classList.add('cell');
     }
 }
