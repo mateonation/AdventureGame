c1f6639c731b557efe78f168ce13978dc4fc04ea
mateonation <mattfernandezrivera@gmail.com>
2024-11-25 18:23:12 +0100

    code were edited to be less complex + 'cell' class replaced for 'ground' and the tiles' size are bigger

--- js/advgame-script.js
+++ js/advgame-script.js
@@ -1,15 +1,14 @@
-let cells=[];
+let tiles=[];
 let gameGrid=document.querySelector('.game-grid');
 const xinitial=-25;const yinitial=12;
 let x;let y;
 let playerisdead=false;
 
-// ELEMENTS FROM EACH LEVEL
-// COMMON VALUES AND ARRAYS
+// DEFINE ELEMENTS FROM EACH LEVEL
 let level=1;
-let spawnpoint=[];
-let goal=[];
-let doorbutton=[];
+let spawnpoint="";
+let goal="";
+let doorbutton="";
 let wall=[];
 let lava=[];
 let door=[];
@@ -19,10 +18,10 @@ window.onload=function(){
     genGame();
 }
 
-// Function to generate the grid cells
+// Function to generate the grid tiles
 function genGame(){
-    cells=[];
-    // Set initial variables for x and y before generating cells
+    tiles=[];
+    // Set initial variables for x and y before generating tiles
     x=xinitial;
     y=yinitial;
     for(i=0;i<1275;i++){
@@ -30,17 +29,11 @@ function genGame(){
         // Add XY position
         div.id=x+"/"+y;
         // Add a class to the div according to the level's number
-        if(doorbutton[level]===div.id){
-            div.classList.add('door-button');
-        }else if(goal[level]===div.id){
-            div.classList.add('goal');
-        }else{
-            div=modifyCell(div,level);
-        }
-        // Push it to the cells array
-        cells.push(div);
-        // Show cell on the screen
-        gameGrid.append(cells[i]);
+        div=modifyTile(div,level);
+        // Push it to the tiles' array
+        tiles.push(div);
+        // Show tile on the screen
+        gameGrid.append(tiles[i]);
         // If X gets to the final element of the row
         if(x===25){
             // Reset X position
@@ -53,26 +46,28 @@ function genGame(){
             x++;
         }
     }
-    // After generating all the cells, locate player's block with the level's spawnpoint
-    locatePlayer(spawnpoint[level]);
+    // After generating all the tiles, locate player's block with the level's spawnpoint
+    locatePlayer(spawnpoint);
     // Show level's number on the bottom section of the page
     let bottomsection=document.getElementsByClassName('bottom-screen')[0];
     bottomsection.textContent='LEVEL: '+level;
 }
 
-// Function to edit cells according to the actual level
-function modifyCell(cell,levelnum){
-    // UNIQUE ELEMENTS FOR EVERY LEVEL
-    spawnpoint=["0/-9","-5/-12"];
-    goal=["-10/7","25/-12"];
-    doorbutton=["-8/-6","-12/-2"];
+// Function to edit tiles according to the actual level
+function modifyTile(tile,levelnum){
+    spawnpoint="";
+    goal="";
+    doorbutton="";
+    wall=[];
+    lava=[];
+    door=[];
     switch(levelnum){
-        // If level number is not included in this switch => it only generates cells
+        // If level number is not included in this switch => it only generates tiles
         default:
             break;
         // Level 0
         case 0:
-            // WALL, LAVA AND DOOR ARRAYS FOR THE LEVEL 0 SHOULD BE ADDED HERE
+            // ALL ELEMENTS OF THE LEVEL SHOULD BE WRITTEN HERE
             wall=[
                 "8/9","9/9","10/9","11/9","12/9","13/9","14/9",
                 "8/8","9/8","10/8","11/8","12/8","13/8","14/8",
@@ -93,60 +88,63 @@ function modifyCell(cell,levelnum){
                 "-16/5","-8/5",
                 "-16/4","-15/4","-14/4","-13/4","-12/4","-11/4","-10/4","-9/4","-8/4"
                 ];
+                spawnpoint="0/-9";
+                doorbutton="-8/-6";
+                goal="-10/7";
             break;
         case 1:
             wall=["-3/9","-2/9","-1/9","0/9","1/9","2/9","3/9","-4/8","-4/7","-1/7","0/7","1/7","2/7","3/7","17/7","18/7","19/7","-4/6","-2/6","3/6","16/6","20/6","-4/5","-2/5","3/5","16/5","20/5","-4/4","-2/4","3/4","8/4","9/4","10/4","11/4","12/4","16/4","20/4","-4/3","-2/3","3/3","7/3","13/3","16/3","20/3","-4/2","-2/2","3/2","7/2","13/2","16/2","19/2","-3/1","3/1","7/1","10/1","13/1","14/1","15/1","16/1","18/1","20/1","-12/0","-11/0","-10/0","3/0","7/0","10/0","18/0","20/0","-13/-1","-9/-1","-6/-1","3/-1","7/-1","10/-1","13/-1","14/-1","18/-1","20/-1","-13/-2","-9/-2","-6/-2","3/-2","7/-2","11/-2","12/-2","15/-2","18/-2","20/-2","-8/-3","-7/-3","-2/-3","-1/-3","0/-3","1/-3","2/-3","7/-3","13/-3","15/-3","18/-3","20/-3","-9/-4","-6/-4","7/-4","10/-4","13/-4","15/-4","18/-4","20/-4","-9/-5","-6/-5","2/-5","3/-5","4/-5","5/-5","6/-5","10/-5","13/-5","15/-5","18/-5","20/-5","-9/-6","-6/-6","0/-6","1/-6","10/-6","13/-6","15/-6","17/-6","20/-6","-9/-7","-6/-7","-1/-7","4/-7","5/-7","6/-7","7/-7","8/-7","9/-7","15/-7","20/-7","-10/-8","-9/-8","-6/-8","-5/-8","-1/-8","10/-8","14/-8","19/-8","-11/-9","-4/-9","0/-9","1/-9","2/-9","3/-9","4/-9","5/-9","6/-9","7/-9","11/-9","12/-9","13/-9","19/-9","-11/-10","-4/-10","10/-10","20/-10","21/-10","22/-10","23/-10","24/-10","25/-10","-11/-11","-6/-11","-5/-11","0/-11","1/-11","2/-11","3/-11","4/-11","5/-11","6/-11","7/-11","8/-11","9/-11","-10/-12","-9/-12","-8/-12","-7/-12","-1/-12"];
             lava=["-11/12","-10/12","-9/12","-8/12","-7/12","-6/12","-5/12","-4/12","-3/12","-2/12","-1/12","-23/11","-22/11","-21/11","-20/11","-12/11","-11/11","-10/11","-9/11","-8/11","-7/11","-6/11","-5/11","-4/11","-3/11","-2/11","-1/11","0/11","1/11","-23/10","-22/10","-21/10","-20/10","-14/10","-13/10","-12/10","-11/10","-10/10","-9/10","-8/10","-7/10","-6/10","-5/10","-4/10","-3/10","-2/10","-1/10","0/10","1/10","2/10","3/10","-23/9","-22/9","-21/9","-20/9","-15/9","-14/9","-13/9","-12/9","-11/9","-10/9","-9/9","-8/9","-7/9","-6/9","-5/9","-23/8","-22/8","-21/8","-20/8","-15/8","-14/8","-13/8","-12/8","-11/8","-10/8","-9/8","-8/8","-7/8","-6/8","-5/8","-23/7","-22/7","-21/7","-20/7","-15/7","-14/7","-13/7","-12/7","-11/7","-10/7","-9/7","-8/7","-7/7","-6/7","-5/7","-23/6","-22/6","-21/6","-20/6","-15/6","-14/6","-13/6","-12/6","-11/6","-10/6","-9/6","-8/6","-7/6","-6/6","-5/6","-23/5","-22/5","-21/5","-20/5","-15/5","-14/5","-13/5","-12/5","-11/5","-10/5","-9/5","-8/5","-7/5","-6/5","-5/5","-23/4","-22/4","-21/4","-20/4","-14/4","-13/4","-12/4","-11/4","-10/4","-9/4","-8/4","-23/3","-22/3","-21/3","-20/3","-23/2","-22/2","-21/2","-20/2","-23/1","-22/1","-21/1","-20/1","-23/0","-22/0","-21/0","-20/0","-23/-1","-22/-1","-21/-1","-20/-1","-23/-2","-22/-2","-21/-2","-20/-2","-11/-2","-23/-3","-22/-3","-21/-3","-20/-3","-12/-3","-11/-3","-23/-4","-22/-4","-21/-4","-20/-4","-16/-4","-15/-4","-12/-4","-11/-4","-23/-5","-22/-5","-21/-5","-20/-5","-17/-5","-16/-5","-15/-5","-14/-5","-12/-5","-11/-5","-23/-6","-22/-6","-21/-6","-20/-6","-18/-6","-17/-6","-16/-6","-15/-6","-14/-6","-13/-6","-12/-6","-23/-7","-22/-7","-21/-7","-20/-7","-19/-7","-18/-7","-17/-7","-16/-7","-15/-7","-14/-7","-13/-7","-23/-8","-22/-8","-21/-8","-20/-8","-19/-8","-18/-8","-17/-8","-16/-8","-15/-8","-14/-8","-13/-8","-23/-9","-22/-9","-21/-9","-20/-9","-19/-9","-18/-9","-17/-9","-16/-9","-15/-9","-14/-9","-23/-10","-22/-10","-21/-10","-20/-10","-19/-10","-18/-10","-17/-10","-16/-10","-15/-10","-23/-11","-22/-11","-21/-11","-20/-11","-19/-11","-18/-11","-17/-11","-16/-11","-15/-11"];
             door=["10/12","10/11","10/10","10/9","10/8","10/7","10/6","7/5","8/5","9/5","10/5","4/4","5/4","6/4","7/4","8/1","9/1"];
+            spawnpoint="-5/-12";
+            goal="25/-12";
+            doorbutton="-12/-2";
             break;
     }
-    // Add 'wall' class to the cell (if it's included on the walls' array associated to it's level)
-    if(wall.includes(cell.id)){
-        cell.classList.add('wall');
+    // Add 'wall' class to the tile (if it's included on the walls' array associated to it's level)
+    if(wall.includes(tile.id)){
+        tile.classList.add('wall');
     }
-    // Add 'lava' class to the cell (if it's included on the lava's array associated to it's level)
-    else if(lava.includes(cell.id)){
-        cell.classList.add('lava');
+    // Add 'lava' class to the tile (if it's included on the lava's array associated to it's level)
+    else if(lava.includes(tile.id)){
+        tile.classList.add('lava');
     }
-    // Add 'door' class to the cell (if it's included on the door's array associated to it's level)
-    else if(door.includes(cell.id)){
-        cell.classList.add('door');
+    // Add 'door' class to the tile (if it's included on the door's array associated to it's level)
+    else if(door.includes(tile.id)){
+        tile.classList.add('door');
     }
-    // Add 'door-button' class to the cell (if it's id coincides with the one almacenated on the level number position of the door button's array)
-    else if(doorbutton[levelnum]===cell.id){
-        cell.classList.add('door-button');
+    // Add 'door-button' class to the tile (if it's id coincides with the one almacenated on the level number position of the door button's array)
+    else if(doorbutton===tile.id){
+        tile.classList.add('door-button');
     }
-    // Add 'goal' class to the cell (if it's id coincides with the one almacenated on the level number position of the goal's array)
-    else if(goal[levelnum]===cell.id){
-        cell.classList.add('goal');
+    // Add 'goal' class to the tile (if it's id coincides with the one almacenated on the level number position of the goal's array)
+    else if(goal===tile.id){
+        tile.classList.add('goal');
     }
-    // Add 'cell' class to the cell if it can be assigned
+    // Add 'ground' class to the tile if it can be assigned
     else{
-        cell.classList.add('cell');
+        tile.classList.add('ground');
     }
-    return cell;
+    // If the spawn is null, set the 0/0 generic one
+    if(spawnpoint===""){
+        spawnpoint="0/0";
+    }
+    return tile;
 }
-// FUNCTION TO LOCATE PLAYER ON THE GRID
+// FUNCTION TO LOCATE PLAYER ON THE GRID EVERY TIME THEY MOVE OR SPAWN
 function locatePlayer(given){
     // Create variable for player's block
-    let playercell;
-    // Generate generic spawn at the center of the grid
-    let genericspawn="0/0";
-    // If the ID 'given' is undefined (this scenario is for a level that does not have a defined spawnpoint)
-    if(given===undefined){
-        given=genericspawn;
-    }
-    playercell=document.getElementById(given);
-    // If the next cell is a door button, it activates the door opening function
+    let playercell=document.getElementById(given);
+    // If the next tile is a door button, it activates the door opening function
     if(playercell.classList.contains('door-button')){
         playercell.classList.remove('door-button');
         // Open the doors of the grid
         openDoor();
     }else{
-        // Remove 'cell' class from the cell the player is going to be
-        playercell.classList.remove('cell');
+        // Remove 'ground' class from the tile the player is going to be
+        playercell.classList.remove('ground');
     }
-    // Placing 'player' class on the cell
+    // Placing 'player' class on the tile
     playercell.classList.add('player');
     // If the player reaches the goal, show alert (WIP - HERE IS WHERE THE NEXT LEVEL WOULD BE TRIGGERED)
     if(playercell.classList.contains('goal')){
@@ -159,8 +157,6 @@ document.body.addEventListener("keydown", (control)=>{
     if(playerisdead){
         return;
     }
-    // Validator of next position to false
-    let canmove=false;
     // Execute when one of these keys are pressed
     if(control.key=='w' || control.key=='a' || control.key=='s' || control.key=='d' || control.key=='W' || control.key=='A' || control.key=='S' || control.key=='D'){
         // Read current position of the player
@@ -174,62 +170,39 @@ document.body.addEventListener("keydown", (control)=>{
             case 'w': case 'W':
                 // Get future position incrementing Y by one
                 futurepos=document.getElementById(x+"/"+(y+1));
-                // Player's block doesn't move if the future position is not valid
-                if(verifyFuturePosition(futurepos)){
-                    // Validator of the next position is true
-                    canmove=true;
-                }else{
-                    canmove=false;
-                }
                 break;
             // Move down
             case 's':case 'S':
                 // Get future position decreasing Y by one
                 futurepos=document.getElementById(x+"/"+(y-1));
-                // Player's block doesn't move if the future position is not valid
-                if(verifyFuturePosition(futurepos)){
-                    // Validator of the next position is true
-                    canmove=true;
-                }else{
-                    canmove=false;
-                }
                 break;
             // Move left
             case 'a':case 'A':
                 // Get future position decreasing X by one
                 futurepos=document.getElementById((x-1)+"/"+y);
-                // Player's block doesn't move if the future position is not valid
-                if(verifyFuturePosition(futurepos)){
-                    // Validator of the next position is true
-                    canmove=true;
-                }else{
-                    canmove=false;
-                }
                 break;
             // Move right
             case 'd': case 'D':
                 // Get future position incrementing X by one
                 futurepos=document.getElementById((x+1)+"/"+y);
-                // Player's block doesn't move if the future position is not valid
-                if(verifyFuturePosition(futurepos)){
-                    // Validator of the next position is true
-                    canmove=true;
-                }else{
-                    canmove=false;
-                }
                 break;
         }
-        if(!canmove){
+        // Player's block doesn't move if the future position is not valid
+        if(verifyFuturePosition(futurepos)){
+            // Player's block can be moved to the future position
+            // Remove player from current cell
+            player.classList.remove('player');
+            // Add 'ground' to that tile
+            player.classList.add('ground');
+            // Locate player on it's future position
+            locatePlayer(futurepos.id);
+            // Locate player on spawn point if they're 'dead'
+            playerDies(futurepos.id);
+            return;
+        }else{
+            // Player's block does not move 'cuz the future position is not valid
             return;
         }
-        // Remove player from current cell
-        player.classList.remove('player');
-        // Add cell to that cell
-        player.classList.add('cell');
-        // Locate player on it's future position
-        locatePlayer(futurepos.id);
-        // Locate player on spawn point if they're 'dead'
-        playerDies(futurepos.id);
     }
 });
 
@@ -238,7 +211,7 @@ function verifyFuturePosition(futurepos){
     // Walk to the future position if it's not null, wall or a door
     if(futurepos!=null && !futurepos.classList.contains('wall') && !futurepos.classList.contains('door')){
         // Change state of player is dead to true if the cell they move to is not a walkable one
-        if(!futurepos.classList.contains('cell') && !futurepos.classList.contains('door-button') && !futurepos.classList.contains('goal')){
+        if(!futurepos.classList.contains('ground') && !futurepos.classList.contains('door-button') && !futurepos.classList.contains('goal')){
             playerisdead=true;
         }
         return true;
@@ -247,7 +220,7 @@ function verifyFuturePosition(futurepos){
     }
 }
 
-// FUNCTION THAT SPAWNS PLAYER ON IT'S SPAWN POINT EVERYTIME THEY WALK INTO A 'DEATH' CELL
+// FUNCTION THAT SPAWNS PLAYER ON IT'S SPAWN POINT EVERYTIME THEY WALK INTO A 'DEATH' TILE
 function playerDies(given){
     // Execute if player is dead
     if(playerisdead){
@@ -256,7 +229,7 @@ function playerDies(given){
             // Remove player from it's position
             player.classList.remove('player');
             // Relocate player on it's spawnpoint
-            locatePlayer(spawnpoint[level]);
+            locatePlayer(spawnpoint);
             // Change it's state to false
             playerisdead=false;
         },400);
@@ -268,6 +241,6 @@ function openDoor(){
     for(i=0;i<door.length;i++){
         let openthisdoor=document.getElementById(door[i]);
         openthisdoor.classList.remove('door');
-        openthisdoor.classList.add('cell');
+        openthisdoor.classList.add('ground');
     }
 }
